0be811ec90f45f7d36f4a747972494a0c04b0483
mick_git <mickeylimalopes@gmail.com>
2019-11-30 14:02:44 -0300

    adding functions prototypes

--- files/lib/function.h
+++ files/lib/function.h
@@ -20,24 +20,35 @@ string search_variable(string var_name);
 string get_current_context_id_label(string user_label, int fixed = 0, int ptrs = 0, int pointsTo = NIL);
 string get_id_label(string user_label, int fixed = 0, int ptrs = 0, int pointsTo = NIL);
 string add_variable_in_current_context(string var_name, int tipo, bool hasTamanho = false);
+string add_iterator_in_current_context(string var_name);
 void initialize_proc_temp_umap();
 int get_new_type(atributos atr_1, atributos atr_2, atributos atr_3);
 string genCountStrLabelStart();
 string genCountStrLabelEnd();
+string genSliceLabelStart();
+string genSliceLabelEnd();
+string genSliceLabelCreate();
+string genSliceLabelHigher();
+string genSliceLabelLower();
+string genAfterSliceLabel();
 void pushContext();
 void endContext();
 string countStringProc();
 string outOfBoundsError();
 void set_error_matrix();
 void set_int_double_matrix();
+void set_boolean_matrix();
+void set_string_matrix();
 void initialize_matrix();
 string string_to_double(string str_label, string double_label);
 string string_to_int(string str_label, string int_label);
-string get_tipo(int tipo, int qtd_ptr = 0);
+string double_to_string(string double_label, string str_label);
+string int_to_string(string int_label, string str_label);
 
 string cmd_label_generator(string cmd_name = "CMD", int desloc = 0);
-string cmd_label_end_generator(string cmd_name = "CMD", int desloc = 0);
 string cmd_label_iter_generator(int desloc = 0);
+string cmd_label_end_generator(string cmd_name = "CMD", int desloc = 0);
+string get_tipo(int tipo, int qtd_ptr = 0);
 string types_operations(atributos& atr_main, atributos atr_1, atributos atr_2, atributos atr_3, int final_type);
 
 void yyerror( string MSG )
